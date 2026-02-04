fad9897bf38b99741605878093b84fd6758d183c
Sofia Rivas <your_email@example.com>
2026-02-04 14:58:58 -0800

    col names

--- FinalProjectScript.R
+++ FinalProjectScript.R
@@ -7,3 +7,8 @@ load(file.path("/Volumes/enhydra/data/students/sofia/zone_level_data.rda"))
 saveRDS(quad_build3, file = "quad_build3.rds")
 
 gonadindex_predictors <- readRDS("quad_build3.rds")
+
+
+write.csv(data.frame(column_names = colnames(gonadindex_predictors)),
+          "column_names.csv",
+          row.names = FALSE)
