6838ef911b08b0f65058bdfac4817ac6559eb00f
jocelynrojas01 <jrojas15@ucsc.edu>
2026-02-04 14:27:29 -0800

    loaded data

--- FinalProjectScript.R
+++ FinalProjectScript.R
@@ -1,8 +1,9 @@
 require(librarian)
-librarian::shelf(tidyverse,here, janitor, googlesheets4, lubridate, splitstackshape,
-                 googledrive,httpuv,dplyr,ggplot2,pwr2,tidyr,broom,ggpubr, paletteer)
+librarian::shelf(tidyverse, here, janitor, googlesheets4, lubridate, splitstackshape,
+                 dplyr,ggplot2,pwr2,tidyr, broom, ggpubr, paletteer)
 
+#don't run these two lines 
 load(file.path("/Volumes/enhydra/data/students/sofia/zone_level_data.rda"))
 saveRDS(quad_build3, file = "quad_build3.rds")
 
-readRDS("quad_build3.rds")
+gonadindex_predictors <- readRDS("quad_build3.rds")
