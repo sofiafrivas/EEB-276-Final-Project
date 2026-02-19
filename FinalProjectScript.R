14cafa432fd6617a15f8cfea218f687642cc129a
Sofia Rivas <your_email@example.com>
2026-02-19 11:46:47 -0800

    tried some modeling (unsuccessfully)

--- FinalProjectScript.R
+++ FinalProjectScript.R
@@ -6,9 +6,16 @@ librarian::shelf(tidyverse, here, janitor, googlesheets4, lubridate, splitstacks
 load(file.path("/Volumes/enhydra/data/students/sofia/zone_level_data.rda"))
 saveRDS(quad_build3, file = "quad_build3.rds")
 
-gonadindex_predictors <- readRDS("quad_build3.rds")
+gonadindex_raw <- readRDS("quad_build3.rds")
 
 
-write.csv(data.frame(column_names = colnames(gonadindex_predictors)),
-          "column_names.csv",
-          row.names = FALSE)
+gi_predictors <- gonadindex_raw %>%
+  mutate(site_id = paste(site, pred_patch, zone, year, sep = " ")) %>%
+  select(-c(patch_id, latitude, longitude, survey_date, site, site_type,
+            pred_patch,patch_cat, zone, year, sd_biomass, sd_gi, se_gonad_mass_g, geometry))
+
+
+model <- glm(mean_gi ~ ., data = gi_predictors, family = gaussian())
+summary(model)
+
+glm(mean_gi ~ macr, data = gi_predictors, family = gaussian())
